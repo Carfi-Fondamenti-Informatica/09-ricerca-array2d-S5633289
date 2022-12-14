643fe112af7e9494b65d348ef1fc253b3749a889
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-14 14:00:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,13 @@
 #include "lib.h"
 
-bool funzione(char Array[10][20], char Array2[20],int &pos) {
+bool funzione(char Array[10][20], char Array2[20], int &pos) {
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 20; j++) {
             if (Array2[j] != Array[i][j]) {
                 j = 20;
             } else {
-                pos = i+1;
+                pos = i + 1;
                 return true;
             }
         }
