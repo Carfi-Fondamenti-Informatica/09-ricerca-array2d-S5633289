3f3f76c9887c076dc5eaa0724e85244b290269c2
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-14 13:45:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,21 @@ int main() {
     char Array2[20];
 
     for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            Array[i][j] = ' ';
+            Array2[j] = ' ';
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
         cin >> Array[i];
     }
 
     cin >> Array2;
-
-    funzione(Array, Array2);
+    int a = funzione(Array, Array2);
+    if (a != -1) {
+        cout << a;
+    } else { cout << "non presente"; }
 
     return 0;
 }
