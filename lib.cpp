45481f398444cc719b020c31d42aa2a8806c211c
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-14 12:26:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
+#include "cstring"
 
+using namespace std;
+
+void funzione(char Array[10][20], char Array2[20]) {
+    char a = ' ';
+    for (int i = 0; i <= 10; i++) {
+        if (Array[i] == Array2) {
+            a = i;
+        }
+    }
+    if (a != ' ') {
+        cout << a;
+    } else { cout << 'non presente'; }
+
+}
