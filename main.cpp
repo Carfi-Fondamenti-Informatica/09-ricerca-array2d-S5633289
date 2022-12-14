458f5dd9ea1b304607d2fae9b8b6301a28ba1832
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-14 12:26:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include "lib.h"
+#include "cstring"
 
-int main(){
-  return 0;
+using namespace std;
+
+void funzione(char Array[10][20], char Array2[20]) {
+    char a = ' ';
+    for (int i = 0; i <= 10; i++) {
+        if (Array[i] == Array2) {
+            a = i;
+        }
+    }
+    if (a != ' ') {
+        cout<< a;
+    } else { cout<< 'non presente'; }
+
+}
+
+int main() {
+    char Array[10][20];
+    char Array2[20];
+    for (int i = 0; i < 10; i++) {
+        cin >> Array[i];
+    }
+    cin >> Array2;
+    funzione(Array, Array2);
+
+    return 0;
 }
