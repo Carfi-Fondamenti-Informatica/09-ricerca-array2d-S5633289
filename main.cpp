f59403e4b3813c5eae9aeaf0ebf66307377e3a84
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-14 14:26:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,7 @@ int main() {
     char Array[10][20];
     char Array2[20];
     int pos = 0;
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 20; j++) {
-            Array[i][j] = ' ';
-            Array2[j] = ' ';
-        }
-    }
-
+    
     for (int i = 0; i < 10; i++) {
         cin >> Array[i];
     }
